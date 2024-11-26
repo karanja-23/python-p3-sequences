a875c1519979c31ec9f850544272f549d985ca30
Hosea <karanja.mungai23@gmail.com>
2024-11-26 20:09:43 +0300

    modified conditionals

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = [0, 1]
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])    
+    else:
+        for i in range(2, length):
+            my_list.append(my_list[i-1] + my_list[i-2])
+        print(my_list)     
+
+print(print_fibonacci(10))  
